7d4cc7a688874e26f5ed11f9e6ec34ba1c8a4096
Alex Sandro <geniofuturo@gmail.com>
2017-05-01 11:28:58 -0300

    Implemented view that returns data about the currently active VPN.

--- openvpn_control/urls.py
+++ openvpn_control/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
 
-from .views import VPNActivateView
+from .views import VPNActivateView, VPNStatusView
 
 
 urlpatterns = [
     url("activate/(?P<pk>\d+)", VPNActivateView.as_view(),
-        name='vpn-control-activate')
+        name='vpn-control-activate'),
+
+    url("status/(?P<pk>\d+)", VPNStatusView.as_view(),
+        name='vpn-control-status')
 ]
--- openvpn_control/views.py
+++ openvpn_control/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from django.core.management import call_command
 from .models import Ovpn
+import ipgetter
 
 
 class VPNActivateView(View):
-
     def get(self, request, pk):
         Ovpn.objects.exclude(pk=pk).update(activated=False)
         Ovpn.objects.filter(pk=pk).update(activated=True)
         call_command("supervisor", **{'ctl-command': ('restart', 'openvpn')})
         return HttpResponse("OK")
+
+
+class VPNStatusView(View):
+    def get(self, request):
+        """Returns data about the currently configured vpn."""
+        return JsonResponse({
+            'current_ip': ipgetter.myip()
+        })

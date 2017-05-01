9d572a158e6fc6d521b26547f623cbdf1a1261a9
Alex Sandro <geniofuturo@gmail.com>
2017-05-01 14:24:55 -0300

    Fix config file.

--- openvpn_control/management/commands/openvpn.py
+++ openvpn_control/management/commands/openvpn.py
@@ -18,25 +18,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """"""
         ovpn = Ovpn.objects.filter(activated=True)
-
         if ovpn.exists():
-            # root always required
-            try:
-                sh.killall("openvpn")
-            except sh.ErrorReturnCode:
-                pass  # no process
             ovpn = ovpn[0]
             auth_filepath = os.path.join(settings.BASE_DIR, "vpn{0.vpn.pk}.auth.txt".format(ovpn))
             with open(auth_filepath, "w") as auth:
                 auth.write(ovpn.vpn.username + '\n')
                 auth.write(ovpn.vpn.password)
-            with open(ovpn.file.path, "r+") as vpn:
+            # get file content
+            with open(ovpn.file.path, "r") as vpn:
                 vpn_file_content = vpn.readlines()
-                for index, line in enumerate(vpn_file_content):
-                    if re.match(self.vpn_param + '.*', line):
-                        vpn_file_content[index] = "{0.vpn_param} {1:s}\n".format(self, auth_filepath)
-                        break
-                vpn.seek(0)
+            # change file
+            for index, line in enumerate(vpn_file_content):
+                if re.match(self.vpn_param + '.*', line):
+                    vpn_file_content[index] = "{0.vpn_param} {1:s}\n".format(self, auth_filepath)
+                    break
+            # write new data
+            with open(ovpn.file.path, "w") as vpn:
                 vpn.write(''.join(vpn_file_content))
-
+            # vpn activate
             sh.openvpn(ovpn.file.path)
